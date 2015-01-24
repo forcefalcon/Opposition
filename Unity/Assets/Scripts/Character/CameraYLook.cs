5bc1a40dfc4c425eefa1b63920f128b2bf20a12e
Pat Lalonde <plalonde@gmail.com>
2015-01-24 17:44:19 -0500

    Added non-rift camera.

---
+++ Unity/Assets/Scripts/Character/CameraYLook.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraYLook : MonoBehaviour
+{
+	public float MouseYSensitivity = 15F;
+	public float MinimumY = -60F;
+	public float MaximumY = 60F;
+
+	float rotationY = 0F;
+	
+	void Update()
+	{
+		rotationY += Input.GetAxis("Mouse Y") * MouseYSensitivity;
+		rotationY = Mathf.Clamp(rotationY, MinimumY, MaximumY);
+
+		transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+	}
+}
