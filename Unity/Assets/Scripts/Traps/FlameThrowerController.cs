5923b21e268e95ad5c71718019a8f76c8550267e
Maxime Bargiel <mbargiel@example.com>
2015-01-25 07:08:16 -0500

    Add logic and animation to FlameThrowers. Burn, baby burn!

---
+++ Unity/Assets/Scripts/Traps/FlameThrowerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class FlameThrowerController : MonoBehaviour
+{
+	public GameObject VFX_FlameThrowerPrefab;
+	private GameObject _VFX_FlameThrowerInstance;
+	
+	public void Start()
+	{
+		_VFX_FlameThrowerInstance = (GameObject)GameObject.Instantiate(
+			VFX_FlameThrowerPrefab,
+			transform.position,
+			Quaternion.identity);
+		_VFX_FlameThrowerInstance.transform.parent = transform;
+	}
+
+	public enum State
+	{
+		Idle,
+		Triggering,
+		Waiting,
+		Resetting,
+	}
+	
+	void Update()
+	{
+		if (_currentState == State.Resetting)
+		{
+			var particleSystems = _VFX_FlameThrowerInstance.GetComponentsInChildren<ParticleSystem>();
+			if (particleSystems.All(ps => !ps.IsAlive()))
+			{
+				SetState(State.Idle);
+			}
+		}
+	}
+	
+	private State _currentState = State.Idle;
+	
+	public void SetState(State newState)
+	{
+		switch(newState)
+		{
+		case State.Idle:
+			_VFX_FlameThrowerInstance.SetActive(false);
+			break;
+		case State.Triggering:
+			_VFX_FlameThrowerInstance.SetActive(true);
+			break;
+		}
+		
+		_currentState = newState;
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (_currentState == State.Waiting && other.tag == "Player")
+		{
+			Debug.Log("YOU DIED!!!");
+		}
+	}
+}
--- Unity/Assets/Scripts/Traps/FlameThrowerTrapController.cs
+++ Unity/Assets/Scripts/Traps/FlameThrowerTrapController.cs
@@ -1,16 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FlameThrowerTrapController : TrapController {
+	private const float FLAME_TRIGGER_TIME = 0.3f;
+	private const float FLAME_WAIT_TIME = 3.0f;
+	private const float FLAME_COOLDOWN = FLAME_TRIGGER_TIME + FLAME_WAIT_TIME + 1.0f;
+
+	private List<FlameThrowerController> _controllers = new List<FlameThrowerController>();
+	private float _triggerTime = 0.0f;
+	private float _waitTime = 0.0f;
 	
-	protected override GameObject TrapPrefab {
-		get {
+	protected override GameObject TrapPrefab 
+	{
+		get 
+		{
 			return TrapManager.Instance.FlameThrowerTrapPrefab;
 		}
 	}
 
+	protected override void Start()
+	{
+		base.Start();
+		
+		for(int i = 0; i < transform.childCount; ++i)
+		{
+			_controllers.Add(transform.GetChild(i).GetComponent<FlameThrowerController>());
+		}
+	}
+	
+	protected override void Update()
+	{
+		base.Update();
+		
+		if (_triggerTime > 0.0f)
+		{
+			_triggerTime -= Time.deltaTime;
+			
+			if (_triggerTime <= 0.0f)
+			{
+				_waitTime = FLAME_WAIT_TIME;
+				SetControllerStates(FlameThrowerController.State.Waiting);
+			}
+		}
+		else if (_waitTime > 0.0f)
+		{
+			_waitTime -= Time.deltaTime;
+			
+			if (_waitTime <= 0.0f)
+			{
+				SetControllerStates(FlameThrowerController.State.Resetting);
+			}
+		}
+	}
+	
 	protected override bool InternalTryActivate()
 	{
-		throw new System.NotImplementedException();
+		_cooldown = FLAME_COOLDOWN;
+		
+		_triggerTime = FLAME_TRIGGER_TIME;
+		SetControllerStates(FlameThrowerController.State.Triggering);
+		
+		return true;
+	}
+	
+	private void SetControllerStates(FlameThrowerController.State state)
+	{
+		foreach (var controller in _controllers)
+		{
+			controller.SetState(state);
+		}
 	}
 }
+
