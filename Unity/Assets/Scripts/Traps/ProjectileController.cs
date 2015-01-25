e42fc2342614cff49cdb5a204a5461041fc941a2
Pat Lalonde <plalonde@gmail.com>
2015-01-25 09:46:53 -0500

    Animating Arrows.

---
+++ Unity/Assets/Scripts/Traps/ProjectileController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour
+{
+	private bool _shooting = false;
+
+	public void SetShooting(bool isShooting)
+	{
+		if (isShooting)
+		{
+			animation.Play();
+		}
+
+		_shooting = isShooting;
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (_shooting && other.tag == "Player" && ApplicationManager.Instance.IsPlaying)
+		{
+			ApplicationManager.Instance.EndGame(true);
+		}
+	}
+}
