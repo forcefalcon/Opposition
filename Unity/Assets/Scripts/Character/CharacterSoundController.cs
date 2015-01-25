f371f9822af3e60a09820019b425b06bdacbde4a
Pat Lalonde <plalonde@gmail.com>
2015-01-24 22:25:30 -0500

    Hooking up player sounds.

---
+++ Unity/Assets/Scripts/Character/CharacterSoundController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSoundController : MonoBehaviour
+{
+	public string FootstepEvent = "";
+	public float FootstepRate = 2.0f;
+	public string SpeedRTPC = "";
+	public float LowSpeedRTPC = 2.0f;
+	public float HighSpeedRTPC = 8.0f;
+	public string FloorTypeSwitch = "";
+
+	private CharacterController _controller = null;
+	private float _timeSinceFootstep = 0.0f;
+
+	void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+	}
+	
+	void Update() 
+	{
+		if (_controller != null)
+		{
+			float speed = _controller.velocity.magnitude;
+			float speedNorm = Mathf.InverseLerp(LowSpeedRTPC, HighSpeedRTPC, speed) * 100.0f;
+			AkSoundEngine.SetRTPCValue(SpeedRTPC, speedNorm, this.gameObject);
+
+			if (FootstepEvent != null)
+			{
+				RaycastHit hit;
+				if (Physics.Raycast(transform.position + new Vector3(0.0f, 1.0f, 0.0f), Vector3.down, out hit, 1.5f))
+				{
+					string tag = hit.collider.gameObject.tag;
+					if (tag != "Untagged")
+					{
+						AkSoundEngine.SetSwitch(FloorTypeSwitch, tag, this.gameObject);
+					}
+				}
+
+				_timeSinceFootstep += Time.deltaTime * speed;
+
+				if (_timeSinceFootstep > FootstepRate)
+				{
+					AkSoundEngine.PostEvent(FootstepEvent, this.gameObject);
+
+					_timeSinceFootstep -= FootstepRate;
+				}
+			}
+		}
+	}
+}
--- Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,19 +13,81 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_BALLOONPOP2 = 461776654U;
+        static const AkUniqueID AMB_PLAY = 2381502256U;
+        static const AkUniqueID PLAYER_FOOTSTEPS_E = 423994534U;
+        static const AkUniqueID SFX_FEEDBACK_COLLECTIBLES_SHIMMER = 991582462U;
+        static const AkUniqueID SFX_FEEDBACKS_COLLECTIBLES_OPENCHEST = 4279255233U;
+        static const AkUniqueID SFX_TORCHES = 2997044537U;
+        static const AkUniqueID SFX_TRAPS_IMPACTS_FLAMES_E = 2606762442U;
+        static const AkUniqueID SFX_TRAPS_IMPACTS_GUILLOTINE_E = 3779648076U;
+        static const AkUniqueID SFX_TRAPS_IMPACTS_LIQUIDS_E = 2320248713U;
+        static const AkUniqueID SFX_TRAPS_IMPACTS_PROJECTILES_E = 2999265302U;
+        static const AkUniqueID SFX_TRAPS_IMPACTS_SPIKES_E = 1953162899U;
+        static const AkUniqueID SFX_TRAPS_MECHANISMS_FLAMES_E = 924253443U;
+        static const AkUniqueID SFX_TRAPS_MECHANISMS_GUILLOTINE_E = 276062753U;
+        static const AkUniqueID SFX_TRAPS_MECHANISMS_LIQUIDS_E = 3871191030U;
+        static const AkUniqueID SFX_TRAPS_MECHANISMS_PROJECTILES_E = 3766497685U;
+        static const AkUniqueID SFX_TRAPS_MECHANISMS_SPIKES_E = 57082998U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ACCOMPLISHMENT_INPERCENT
+        {
+            static const AkUniqueID GROUP = 3463215277U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HUNDERED_PERCENT = 3305510938U;
+                static const AkUniqueID ZER0_PERCENT = 3183414770U;
+            } // namespace SWITCH
+        } // namespace ACCOMPLISHMENT_INPERCENT
+
+        namespace FLOORTYPE_SW
+        {
+            static const AkUniqueID GROUP = 2500103914U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FLOORTYPE_CONCRETE = 1329506165U;
+                static const AkUniqueID FLOORTYPE_DIRT = 2949268711U;
+                static const AkUniqueID FLOORTYPE_METAL = 3622077093U;
+                static const AkUniqueID FLOORTYPE_WATER = 3095200985U;
+                static const AkUniqueID FLOORTYPE_WOOD = 1128660171U;
+            } // namespace SWITCH
+        } // namespace FLOORTYPE_SW
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ACCOMPLISHMENT = 907394056U;
+        static const AkUniqueID PLAYER_SPEED = 1062779386U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+        static const AkUniqueID ALLSOUNDS = 3278463154U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
 }// namespace AK
