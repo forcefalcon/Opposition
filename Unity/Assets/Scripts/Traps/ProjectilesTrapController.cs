d38f70745210f5c1a2fea584c8be15853172a1e9
Maxime Bargiel <mbargiel@example.com>
2015-01-25 01:17:38 -0500

    Spawn all traps to appropriate place in rooms.
    TODO: "hide" them
    TODO: fix projectile traps to be directional

---
+++ Unity/Assets/Scripts/Traps/ProjectilesTrapController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectilesThrowerTrapController : TrapController {
+	public GameObject ProjectilesThrowerPrefab;
+	
+	protected override GameObject TrapPrefab {
+		get {
+			return ProjectilesThrowerPrefab;
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Traps/SpikesTrapController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesTrapController : TrapController {
+	public GameObject SpikesTrapPrefab;
+	
+	protected override GameObject TrapPrefab {
+		get {
+			return TrapManager.Instance.SpikesTrapPrefab;
+		}
+	}
+}
